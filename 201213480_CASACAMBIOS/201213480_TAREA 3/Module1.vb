f17ba8a7633ddc41a02225097b90643c1e4ccb10
JosePisqui201213480 <98623728+JosePisqui201213480@users.noreply.github.com>
2022-02-12 12:31:39 -0600

    TAREA 3

---
+++ 201213480_CASACAMBIOS/201213480_TAREA 3/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+    Const tcDollar = 7.69
+    Const tcPm = 7.89
+    Const tcColones = 0.012
+    Const ComisionCompra = 2.5 / 100
+    Const ComisionVenta = 
+        Sub= limpiar ()
+    Function calcular(ByVal CantidadCompra As Double, ByVal Cantidadneta As Double)
+        If CantidadCompra > 0 Then
+            Total_parcial = CantidadCompra * tipocompra
+            total = 
+
+        End If
+    End Function
+
+
+
+End Module
